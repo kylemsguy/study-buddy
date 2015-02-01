e25c20e501ecbece9c01c2e4760108889d8e3007
Eric Bannatyne <eric@user3-218-216.wireless.utoronto.ca>
2015-01-31 22:30:07 -0500

    testing for POST requests

--- sb_server/sb_serv/urls.py
+++ sb_server/sb_serv/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, include, url
 from sb_serv import views
 
 urlpatterns = patterns('',
+    url(r'^serv_post/$', views.serv_post, name='serv_post'),
     url(r'^list_users/$', views.list_users, name='list_users'),
-    url(r'^register_user/(?P<user_name>\w+)/(?P<user_email>\w+)$', views.register_user, name='register_user'),
+    url(r'^register_user/$', views.register_user, name='register_user'),
 )
