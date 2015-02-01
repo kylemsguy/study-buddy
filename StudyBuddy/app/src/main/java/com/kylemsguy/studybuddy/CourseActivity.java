6f4b0f05044ef69129a2221a66b37459307ab57e
Akira <akirakakar@gmail.com>
2015-02-01 08:54:57 -0500

    fixed button i think

--- StudyBuddy/app/src/main/java/com/kylemsguy/studybuddy/CourseActivity.java
+++ StudyBuddy/app/src/main/java/com/kylemsguy/studybuddy/CourseActivity.java
@@ -15,9 +15,6 @@ import android.view.MenuItem;
 
 
 import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
-
 
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -26,12 +23,10 @@ import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.gcm.GoogleCloudMessaging;
 
 import com.google.api.services.calendar.model.Event;
-import com.google.api.services.calendar.model.Events;
 
 import com.kylemsguy.studybuddy.backend.ConnectionManager;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 
@@ -80,21 +75,21 @@ public class CourseActivity extends ActionBarActivity implements OnClickListener
 
         //button
         getCalendarEvents();
-        Button one = (Button) findViewById(R.id.one);
+        one = (Button) findViewById(R.id.one);
         one.setText("CSC324");
-        one.setOnClickListener((View.OnClickListener) this);
-        Button four = (Button) findViewById(R.id.four);
+        one.setOnClickListener(this);
+        four = (Button) findViewById(R.id.four);
         four.setText("CSC411");
-        four.setOnClickListener((View.OnClickListener) this);
-        Button two = (Button) findViewById(R.id.two);
+        four.setOnClickListener(this);
+        two = (Button) findViewById(R.id.two);
         two.setText("CSC384");
-        two.setOnClickListener((View.OnClickListener) this);
-        Button three = (Button) findViewById(R.id.three);
+        two.setOnClickListener(this);
+        three = (Button) findViewById(R.id.three);
         three.setText("CSC336");
-        three.setOnClickListener((View.OnClickListener) this);
-        Button zero = (Button) findViewById(R.id.zero);
+        three.setOnClickListener(this);
+        zero = (Button) findViewById(R.id.zero);
         zero.setText("CSC343");
-        zero.setOnClickListener((View.OnClickListener) this);
+        zero.setOnClickListener(this);
 
 
 
@@ -294,7 +289,7 @@ public class CourseActivity extends ActionBarActivity implements OnClickListener
     @Override
     public void onClick(View v) {
         if (v == one ){
-            Intent intent = new Intent(this,ChatActivity.class);
+            Intent intent = new Intent(this, ChatActivity.class);
             startActivityForResult(intent, 0);
         }
         if (v == two ){
@@ -314,4 +309,6 @@ public class CourseActivity extends ActionBarActivity implements OnClickListener
             startActivityForResult(intent, 0);
         }
     }
+
+
 }
